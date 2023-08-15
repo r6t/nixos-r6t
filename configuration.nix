e4de59fc4492d0718fa4f7b78eac368422d7bd95
r6t <ryancast@gmail.com>
2023-08-14 22:37:03 -0700

    starting to configure desktop

--- configuration.nix
+++ configuration.nix
@@ -5,115 +5,97 @@
 
 { imports =
     [ <home-manager/nixos>
-      # <nixos-hardware/framework>
+      <nixos-hardware/framework>
 
       # Include the results of the hardware scan.
       ./hardware-configuration.nix ];
 
-  # Bootloader.
+  ### NIXOS CONFIGURATION
   boot.loader.systemd-boot.enable = true; boot.loader.efi.canTouchEfiVariables = true;
-
-  # Setup keyfile
   boot.initrd.secrets = { "/crypto_keyfile.bin" = null;
   };
-
-  # Enable swap on luks
   boot.initrd.luks.devices."luks-f26077b3-1094-45f1-8c6e-07f7fee52e72".device = "/dev/disk/by-uuid/f26077b3-1094-45f1-8c6e-07f7fee52e72"; 
   boot.initrd.luks.devices."luks-f26077b3-1094-45f1-8c6e-07f7fee52e72".keyFile = "/crypto_keyfile.bin";
 
-  # Networking
+  environment.shells = with pkgs; [ zsh ]; # /etc/shells
+
+  hardware.bluetooth.enable = true;
+  hardware.pulseaudio.enable = false; # Just say no to pulseaudio
+
   networking.networkmanager.enable = true;
-  networking.hostName = "mountainball"; # Define your hostname.
-  # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
+  networking.hostName = "mountainball";
+  # networking.wireless.enable = true; # wpa_supplicant
 
-  # Configure network proxy if necessary networking.proxy.default = "http://user:password@proxy:port/"; networking.proxy.noProxy = 
-  # "127.0.0.1,localhost,internal.domain";
-
-  # Time zone.
-  time.timeZone = "America/Los_Angeles";
-
-  # Internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = { LC_ADDRESS = "en_US.UTF-8"; LC_IDENTIFICATION = "en_US.UTF-8"; LC_MEASUREMENT = "en_US.UTF-8"; LC_MONETARY = 
     "en_US.UTF-8"; LC_NAME = "en_US.UTF-8"; LC_NUMERIC = "en_US.UTF-8"; LC_PAPER = "en_US.UTF-8"; LC_TELEPHONE = "en_US.UTF-8"; LC_TIME = 
     "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true; services.xserver.desktopManager.plasma5.enable = true;
-
-  # Get our flatpak on
-  services.flatpak.enable = true;
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-
-  # Configure keymap in X11
-  services.xserver = { layout = "us"; xkbVariant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true; hardware.pulseaudio.enable = false; security.rtkit.enable = true; services.pipewire = {
-    enable = true; alsa.enable = true; alsa.support32Bit = true; pulse.enable = true;
-    # If you want to use JACK applications, uncomment this jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default, no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager). services.xserver.libinput.enable = true;
-
-  # "always enable the shell system-wide"
+  programs.hyprland.enable = false;
   programs.zsh.enable = true;
 
-  # hyprland
-  # programs.hyprland.enable = true;
+  sound.enable = true; # Enable sound with pipewire
 
-  # /etc/shells the nixos way
-  environment.shells = with pkgs; [ zsh ];
+  security.rtkit.enable = true;
+
+  time.timeZone = "America/Los_Angeles";
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-wlr pkgs.xdg-desktop-portal-gtk ];
 
   ### USER + APPLICATIONS
   # Probably should be managing the user itself via home-manager?
   users.users.user = { isNormalUser = true; description = "user"; extraGroups = [ "networkmanager" "wheel" ]; shell = pkgs.zsh;
-  #packages = with pkgs; [
-  #    ansible
-  #    firefox
-  #    freetube
-  #    kate
-  #    ripgrep
-  #  #  thunderbird
-  #  ];
+  # packages managed via home-manager
   };
   #home.username = "user"
   #home.homeDirectory = "/home/user"
 
   home-manager.users.user = { pkgs, ...}: {
     home.packages = [
-      pkgs.alacritty
       pkgs.ansible
       pkgs.brave
-      pkgs.firefox
+      pkgs.firefox-wayland
       pkgs.freecad
       pkgs.freetube
       pkgs.kate
       pkgs.mullvad-vpn
+      pkgs.neofetch
       pkgs.librewolf
       pkgs.ripgrep
       pkgs.signal-desktop
+      pkgs.thefuck
       pkgs.ungoogled-chromium
       pkgs.virt-manager
       pkgs.vlc
+      pkgs.youtube-dl
+
     ];
+    programs.alacritty = {
+      enable = true;
+      settings = {
+        font = {
+	  size = 14.0;
+	};
+        selection = {
+	  save_to_clipboard = true;
+	};
+      };
+    };
     programs.git = {
       enable = true;
       userName = "r6t";
       userEmail = "ryancast@gmail.com";
+      extraConfig = {
+        core = {
+          editor = "nvim";
+        };
+      };
+      ignores = [
+        ".DS_Store"
+        "*.pyc"
+      ];
     };
     programs.vscode = {
       enable = true;
@@ -131,6 +113,9 @@
 	  plugins = [ "aws" "git" "python" "thefuck" ];
           theme = "xiong-chiamiov-plus";
       };
+    };
+    home.sessionVariables = {
+        MOZ_ENABLE_WAYLAND = 1;
     };
     home.stateVersion = "23.05";
   };
@@ -150,7 +135,20 @@
   # };
 
   ### SERVICES:
-  services.fwupd.enable = true;
+  services.flatpak.enable = true;
+  services.fwupd.enable = true; # Linux firmware updater
+  services.mullvad-vpn.enable = false; # Mullvad desktop app
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default, no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+  services.printing.enable = true; # CUPS print support
   services.syncthing = {
     enable = true;
     dataDir = "/home/user"; # unused value
@@ -160,16 +158,19 @@
     group = "users";
     guiAddress = "0.0.0.0:8384";
   };
-  services.mullvad-vpn.enable = false;
+  services.xserver.enable = true;
+  services.xserver = { layout = "us"; xkbVariant = ""; }; # X11 keymap
+  services.xserver.displayManager.sddm.enable = true; # KDE Plasma
+  services.xserver.desktopManager.plasma5.enable = true; # KDE Plasma
+  services.xserver.displayManager.defaultSession = "plasmawayland"; # KDE Plasma
+  # services.xserver.libinput.enable = true; # Enable touchpad support (enabled default in most desktopManager).
 
   # Enable the OpenSSH daemon. services.openssh.enable = true;
 
   # Open ports in the firewall. networking.firewall.allowedTCPPorts = [ ... ]; networking.firewall.allowedUDPPorts = [ ... ]; Or disable the firewall 
   # altogether. networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default settings for stateful data, like file locations and database versions on your 
-  # system were taken. It‘s perfectly fine and recommended to leave this value at the release version of the first install of this system. Before 
-  # changing this value read the documentation for this option (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  # Before changing this value read the documentation for this option (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05";
 
 }
