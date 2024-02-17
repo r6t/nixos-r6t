3667fdcd7ca05fc301ccbd69e34924ed14e56c69
r6t <ryancast@gmail.com>
2024-02-17 13:28:23 -0800

    libvirtd

--- beehive.nix
+++ beehive.nix
@@ -85,6 +85,22 @@
 
   # System services:
   services.fwupd.enable = true; # Linux firmware updater
+  services.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.unstable.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
+
   services.syncthing = {
     enable = true;
     dataDir = "/home/r6t/Sync";
