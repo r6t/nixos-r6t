4a232d2b27f5ad80c0315a18af091be30be95ca5
r6t <ryancast@gmail.com>
2024-02-23 16:57:06 -0800

    OSX-KVM boot options

--- nixos/beehive-configuration.nix
+++ nixos/beehive-configuration.nix
@@ -80,6 +80,12 @@ in
     };
   };
 
+  # Boot options for OSX-KVM
+  boot.extraModprobeConfig = ''
+    options kvm_intel nested=1
+    options kvm_intel emulate_invalid_guest_state=0
+    options kvm ignore_msrs=1
+  '';
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
