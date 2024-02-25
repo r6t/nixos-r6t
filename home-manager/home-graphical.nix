d86fc7e21c9e7ab662a0d10c10942a31bdf65401
r6t <ryancast@gmail.com>
2024-02-25 10:09:27 -0800

    rm nixvim config

--- home-manager/home-graphical.nix
+++ home-manager/home-graphical.nix
@@ -175,57 +175,57 @@
   #      };
   #    };
 
-      plugins.nvim-cmp = {
-        enable = true;
-        autoEnableSources = true;
-        snippet = { expand = "luasnip"; };
+  #     plugins.nvim-cmp = {
+  #       enable = true;
+  #       autoEnableSources = true;
+  #       snippet = { expand = "luasnip"; };
 
-        sources = [
-          { name = "nvim_lsp"; }
-          { name = "luasnip"; }
-          { name = "buffer"; }
-          { name = "nvim_lua"; }
-          { name = "path"; }
-        ];
+  #       sources = [
+  #         { name = "nvim_lsp"; }
+  #         { name = "luasnip"; }
+  #         { name = "buffer"; }
+  #         { name = "nvim_lua"; }
+  #         { name = "path"; }
+  #       ];
 
-        mapping = {
-          "<CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })";
-          "<Tab>" = {
-            modes = [ "i" "s" ];
-            action =
-              # lua
-              ''
-                function(fallback)
-                  if cmp.visible() then
-                    cmp.select_next_item()
-                  elseif require("luasnip").expand_or_jumpable() then
-                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-                  else
-                    fallback()
-                  end
-                end
-              '';
-          };
-          "<S-Tab>" = {
-            modes = [ "i" "s" ];
-            action =
-              # lua
-              ''
-                function(fallback)
-                  if cmp.visible() then
-                    cmp.select_prev_item()
-                  elseif require("luasnip").jumpable(-1) then
-                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-                  else
-                    fallback()
-                  end
-                end
-              '';
-          };
+  #       mapping = {
+  #         "<CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })";
+  #         "<Tab>" = {
+  #           modes = [ "i" "s" ];
+  #           action =
+  #             # lua
+  #             ''
+  #               function(fallback)
+  #                 if cmp.visible() then
+  #                   cmp.select_next_item()
+  #                 elseif require("luasnip").expand_or_jumpable() then
+  #                   vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+  #                 else
+  #                   fallback()
+  #                 end
+  #               end
+  #             '';
+  #         };
+  #         "<S-Tab>" = {
+  #           modes = [ "i" "s" ];
+  #           action =
+  #             # lua
+  #             ''
+  #               function(fallback)
+  #                 if cmp.visible() then
+  #                   cmp.select_prev_item()
+  #                 elseif require("luasnip").jumpable(-1) then
+  #                   vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+  #                 else
+  #                   fallback()
+  #                 end
+  #               end
+  #             '';
+  #         };
 
-        };
-      };
-  };
+  #       };
+  #     };
+  # };
   programs.alacritty = {
     enable = true;
     settings = {
