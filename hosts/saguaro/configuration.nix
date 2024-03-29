d4cece415740044e91a42fbffbbd93046fd7c688
r6t <ryancast@gmail.com>
2024-03-28 19:26:25 -0700

    saguaro to modules

--- hosts/saguaro/configuration.nix
+++ hosts/saguaro/configuration.nix
@@ -11,37 +11,27 @@ let
   inherit (inputs) ssh-keys;
 in
  {
-  # You can import other NixOS modules here
   imports = [
     inputs.home-manager.nixosModules.home-manager
-    # If you want to use modules from other flakes (such as nixos-hardware):
-    # inputs.hardware.nixosModules.common-cpu-amd
-    # inputs.hardware.nixosModules.common-ssd
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./users.nix
-
-    # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
+    ../../modules/base-system.nix
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
+  # apps modules
+  mine.docker.enable = true;
+  mine.flatpak.enable = true;
+  mine.netdata.enable = true;
+  mine.ssh.enable = true;
+  mine.syncthing.enable = true;
+  mine.tailscale.enable = true;
+  mine.zsh.enable = true;
 
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    config = {
-      allowUnfree = true;
-    };
-  };
+  # system modules
+  mine.env.enable = true;
+  mine.fwupd.enable = true;
+  mine.localization.enable = true;
+  mine.nix.enable = true;
+  mine.nixpkgs.enable = true;
 
   home-manager = {
     extraSpecialArgs = { inherit inputs outputs; };
@@ -51,61 +41,9 @@ in
     };
   };
 
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-  # This will additionally add your inputs to the system's legacy channels
-  # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = ["/etc/nix/path"];
-  environment.etc =
-    lib.mapAttrs'
-    (name: value: {
-      name = "nix/path/${name}";
-      value.source = value.flake;
-    })
-    config.nix.registry;
-
-
-  nix = {
-    # NixOS garbage collection
-    gc = {
-      automatic = true;
-      dates = "monthly";
-      options = "--delete-older-than-60d";
-    };
-    settings = {
-      auto-optimise-store = true;
-      experimental-features = "nix-command flakes";
-    };
-  };
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  environment.shells = with pkgs; [ zsh ]; # /etc/shells
-  # System packages
-  environment.systemPackages = with pkgs; [
-     ansible
-     curl
-     fd
-     git
-     lshw
-     neovim
-     neofetch
-     nmap
-     nodejs
-     pciutils
-     ripgrep
-     tmux
-     unzip
-     usbutils
-     wget
-     tree
-  ];
-
-  hardware.bluetooth.enable = false;
 
   networking.firewall.allowedTCPPorts = [ 
     22
@@ -118,53 +56,6 @@ in
   networking.hostName = "saguaro";
   networking.networkmanager.enable = true;
 
-  programs.zsh.enable = true;
-
-  time.timeZone = "America/Los_Angeles";
-
-  # Internationalization 
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # System services:
-  services.fwupd.enable = false; # Linux firmware updater
-  services.syncthing = {
-    enable = true;
-    dataDir = "/home/r6t/Sync";
-    openDefaultPorts = true;
-    overrideDevices = false;
-    overrideFolders = false;
-    configDir = "/home/r6t/.config/syncthing";
-    user = "r6t";
-    group = "users";
-    guiAddress = "0.0.0.0:8384";
-  };
-  services.tailscale.enable = true;
-  # Configure keymap in X11
-  services.xserver = {
-    xkb = {
-      layout = "us";
-      variant = "";
-    };
-  };
-
-  services.openssh = {
-    enable = true;
-      # PermitRootLogin = "no";
-      # PasswordAuthentication = true;
-    };
-
   system.stateVersion = "23.11";
 
   users.users = {
@@ -173,19 +64,6 @@ in
       openssh.authorizedKeys.keyFiles = [ ssh-keys.outPath ];
       extraGroups = [ "docker" "networkmanager" "wheel"];
       shell = pkgs.zsh;
-    };
-  };
-
-  # Containers
-  virtualisation.docker = { 
-    daemon.settings = {
-      data-root = "/home/r6t/docker-root";
-    };
-    enable = true;
-    enableOnBoot = true;
-    rootless = {
-      enable = true;
-      setSocketVariable = true;
     };
   };
 }
