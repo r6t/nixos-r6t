83fda8ddaab3f351956b43ea9c9bf929d46aecc6
r6t <ryancast@gmail.com>
2024-03-31 17:58:02 -0700

    mountainball to modules

--- hosts/mountainball/configuration.nix
+++ hosts/mountainball/configuration.nix
@@ -11,41 +11,37 @@ let
   inherit (inputs) ssh-keys;
 in
 {
-  # You can import other NixOS modules here
   imports = [
     inputs.home-manager.nixosModules.home-manager
-    # If you want to use modules from other flakes (such as nixos-hardware):
-    # inputs.hardware.nixosModules.common-cpu-amd
-    # inputs.hardware.nixosModules.common-ssd
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./users.nix
-
-    # Import your generated (nixos-generate-config) hardware configuration
+    inputs.hardware.nixosModules.common.cpu.amd
+    inputs.hardware.nixosModules.common.gpu.nvidia
     ./hardware-configuration.nix
+    ../../modules/base-system.nix
+    ../../modules/desktop-workstation.nix
   ];
 
+  # apps modules
+  mine.docker.enable = true;
+  mine.flatpak.enable = true;
+  mine.hypr.enable = true;
+  mine.mullvad.enable = true;
+  mine.netdata.enable = true;
+  mine.ssh.enable = true;
+  mine.syncthing.enable = true;
+  mine.tailscale.enable = true;
+  mine.zsh.enable = true;
 
-
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-    };
-  };
+  # system modules
+  mine.bluetooth.enable = true;
+  mine.bolt.enable = true;
+  mine.env.enable = true;
+  mine.fonts.enable = true;
+  mine.fwupd.enable = true;
+  mine.localization.enable = true;
+  mine.nix.enable = true;
+  mine.nixpkgs.enable = true;
+  mine.printing.enable = true;
+  mine.sound.enable = true;
 
   home-manager = {
     extraSpecialArgs = { inherit inputs outputs; };
@@ -55,92 +51,11 @@ in
     };
   };
 
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-  # This will additionally add your inputs to the system's legacy channels
-  # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = ["/etc/nix/path"];
-  environment.etc =
-    lib.mapAttrs'
-    (name: value: {
-      name = "nix/path/${name}";
-      value.source = value.flake;
-    })
-    config.nix.registry;
-
-
-  nix = {
-    # NixOS garbage collection
-    gc = {
-      automatic = true;
-      dates = "monthly";
-      options = "--delete-older-than-60d";
-    };
-    settings = {
-      auto-optimise-store = true;
-      experimental-features = "nix-command flakes";
-    };
-  };
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.luks.devices."luks-ca693f0d-4d0a-4eee-ba6a-fdc2db22dfb1".device = "/dev/disk/by-uuid/ca693f0d-4d0a-4eee-ba6a-fdc2db22dfb1";
   boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ]; # sleep/wake
-
-  environment.sessionVariables = {
-    # Electron hint
-    NIXOS_OZONE_WL = "1";
-    QT_STYLE_OVERRIDE = "Breeze-Dark"; # maybe not needed 
-    # Wayland Nvidia disappearing cursor fix
-    WLR_NO_HARDWARE_CURSORS = "1";
-
-  };
-  environment.shells = with pkgs; [ zsh ]; # /etc/shells
-  # System packages
-  environment.systemPackages = with pkgs; [
-     ansible
-     curl
-     docker-compose
-     fd
-     git
-     home-manager
-     libva # https://wiki.hyprland.org/hyprland-wiki/pages/Nvidia/
-     lshw
-     neovim
-     neofetch
-     nmap
-     nodejs
-     nvidia-docker
-     pciutils
-     ripgrep
-     tmux
-     unzip
-     usbutils
-     wget
-     tree
-  ];
-
-  fonts = {
-    fontDir.enable = true;
-    packages = with pkgs; [
-      noto-fonts-emoji
-      font-awesome
-      hack-font
-      nerdfonts
-      source-sans-pro
-    ];
-  };
-
-  hardware.bluetooth.enable = true;
-  # Experimental settings allow the os to read bluetooth device battery level
-  hardware.bluetooth.settings = {
-    General = {
-      Experimental = true;
-     };
-  };
 
   # Nvidia GPU (unfree)
   hardware.opengl = {
@@ -161,75 +76,6 @@ in
   networking.networkmanager.enable = true;
   networking.firewall.allowedTCPPorts = [ 22 3000 11434 ];
 
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-  programs.zsh.enable = true;
-
-  security.pam.services.swaylock = {}; # required for swaylock-effects functionality
-  security.polkit.enable = true; # hyprland support
-  security.rtkit.enable = true; # sound
-
-  time.timeZone = "America/Los_Angeles";
-
-  # Internationalization 
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # System services:
-  services.blueman.enable = true; # Bluetooth
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-  services.flatpak.enable = true;
-  services.fprintd.enable = false; # causing nix build error 3/22/24
-  services.fwupd.enable = true; # Linux firmware updater
-  services.mullvad-vpn.enable = true; # Mullvad desktop app
-  services.printing.enable = true; # CUPS print support
-  services.syncthing = {
-    enable = true;
-    dataDir = "/home/r6t/icloud";
-    openDefaultPorts = true;
-    overrideDevices = false;
-    overrideFolders = false;
-    configDir = "/home/r6t/.config/syncthing";
-    user = "r6t";
-    group = "users";
-    guiAddress = "127.0.0.1:8384";
-  };
-  services.tailscale.enable = true;
-  # Configure keymap in X11
-  services.xserver = {
-    videoDrivers = ["nvidia"];
-    xkb = {
-      layout = "us";
-      variant = "";
-    };
-  };
-
-  sound.enable = true; # see services.pipewire
-
-  services.openssh = {
-    enable = true;
-      # PermitRootLogin = "no";
-      # PasswordAuthentication = true;
-    };
-
   system.stateVersion = "23.11";
 
   users.users = {
@@ -240,26 +86,5 @@ in
       extraGroups = [ "docker" "input" "networkmanager" "wheel"];
       shell = pkgs.zsh;
     };
-  };
-
-  # Containers
-  virtualisation.docker = { 
-    daemon.settings = {
-      data-root = "/home/r6t/docker-root";
-    };
-    enable = true;
-    enableOnBoot = true;
-    rootless = {
-      enable = true;
-      setSocketVariable = true;
-    };
-  };
-
-  # Desktop portal
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    # gtk portal needed to make gtk apps happy
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 }
