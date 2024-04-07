00b270c47c73a083199369497f799b6aa6bc3c75
r6t <ryancast@gmail.com>
2024-04-07 10:20:01 -0700

    block unfree in top level home module

--- home-manager/home-graphical.nix
+++ home-manager/home-graphical.nix
@@ -12,16 +12,6 @@
   imports = [
   ];
 
-  nixpkgs = {
-    overlays = [
-    ];
-    config = {
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
-    };
-  };
-
 
   home = {
     homeDirectory = "/home/r6t";
