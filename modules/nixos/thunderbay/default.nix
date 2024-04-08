f301603a3de362fecd93291a9826e522a541c654
r6t <ryancast@gmail.com>
2024-04-08 11:33:32 -0700

    wip tbay luks via uuid

--- modules/nixos/thunderbay/default.nix
+++ modules/nixos/thunderbay/default.nix
@@ -9,7 +9,11 @@
       environment.etc.crypttab = {
         enable = true;
         text = ''
+          8TB-A1 UUID=3c429d84-386d-4272-8739-7bd2dcde1159 /home/r6t/luks-keys/tbay/8tba luks
+          8TB-D1 UUID=5b66a482-036d-4a76-8cec-6ad15fe2360c /home/r6t/luks-keys/tbay/8tbb luks
           8TB-C1 UUID=cb067a1e-147b-4052-b561-e2c16c31dd0e /home/r6t/luks-keys/tbay/8tbd luks
+          4TB-B1 UUID=b214dac6-7a73-4e53-9f89-b1ae82c0c625 /home/r6t/luks-keys/tbay/4tbe luks
+          2TB-E1 UUID=544de6c8-1332-47d2-a38f-ed67d4db46e4 /home/r6t/luks-keys/tbay/2tbf luks
         '';
       };
 
