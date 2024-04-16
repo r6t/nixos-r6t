f88ea125985df280b0cba4615daacaf74bb9aa79
r6t <ryancast@gmail.com>
2024-04-16 12:21:45 -0700

    wip specify oci backend

--- modules/nixos/selfhost/default.nix
+++ modules/nixos/selfhost/default.nix
@@ -9,18 +9,21 @@
   };
 
   config = lib.mkIf config.mine.selfhost.enable {
-    virtualisation.oci-containers.containers = {
-      jellyfin = {
-        image = "jellyfin/jellyfin";
-        ports = [ "8096:8096" ];
-        volumes = [
-          "/home/r6t/external-ssd/2TB-E/config/jellyfin:/config"
-          "/home/r6t/external-ssd/2TB-E/cache/jellyfin:/cache"
-          "/home/r6t/external-ssd/8TB-A/movies:/media/movies"
-          "/home/r6t/external-ssd/8TB-D/storage/plex/music:/media/music"
-          "/home/r6t/external-ssd/8TB-D/storage/plex/tv:/media/tv"
-        ];
-        extraOptions = [ "--user=1000:100" ];
+    virtualisation.oci-containers = {
+      backend = "docker";
+      containers = {
+        jellyfin = {
+          image = "jellyfin/jellyfin";
+          ports = [ "8096:8096" ];
+          volumes = [
+            "/home/r6t/external-ssd/2TB-E/config/jellyfin:/config"
+            "/home/r6t/external-ssd/2TB-E/cache/jellyfin:/cache"
+            "/home/r6t/external-ssd/8TB-A/movies:/media/movies"
+            "/home/r6t/external-ssd/8TB-D/storage/plex/music:/media/music"
+            "/home/r6t/external-ssd/8TB-D/storage/plex/tv:/media/tv"
+          ];
+          extraOptions = [ "--user=1000:100" ];
+        };
       };
     };
   };
