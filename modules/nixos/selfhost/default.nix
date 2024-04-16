c0b2cfe07e3cec5ba60ea845d31a2fbfdda9789b
r6t <ryancast@gmail.com>
2024-04-16 11:38:16 -0700

    wip container options formatting

--- modules/nixos/selfhost/default.nix
+++ modules/nixos/selfhost/default.nix
@@ -20,7 +20,7 @@
           "/home/r6t/external-ssd/8TB-D/storage/plex/music:/media/music"
           "/home/r6t/external-ssd/8TB-D/storage/plex/tv:/media/tv"
         ];
-        extraOptions = "--user=1000:100";
+        extraOptions = [ "--user=1000:100" ];
       };
     };
   };
