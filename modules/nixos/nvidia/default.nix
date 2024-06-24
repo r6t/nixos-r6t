cb0fc5b47a0e19cb8062f53735291b87a9f0167d
r6t <ryancast@gmail.com>
2024-06-24 12:03:35 -0700

    compatibility update

--- modules/nixos/nvidia/default.nix
+++ modules/nixos/nvidia/default.nix
@@ -22,10 +22,9 @@
       };
       environment.systemPackages = with pkgs; [ libva ];
 
-      hardware.opengl = {
+      hardware.graphics = {
         enable = true;
-        driSupport = true;
-        driSupport32Bit = true;
+        enable32Bit = true;
       };
 
       hardware.nvidia = {
@@ -35,25 +34,7 @@
         open = false;
         nvidiaSettings = true;
         # package = config.boot.kernelPackages.nvidiaPackages.stable;
-        # package = config.boot.kernelPackages.nvidiaPackages.beta; it's still 550
-        ###
         package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-          #version = "535.154.05";
-          #sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
-          #sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
-          #openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
-          #settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
-          #persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
-
-          #version = "550.40.07";
-          #sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
-          #sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
-          #openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
-          #settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
-          #persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
-          # This was needed for 535 and 550
-          #patches = [ rcu_patch ];
-
           version = "555.42.02";
           sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
           sha256_aarch64 = lib.fakeSha256;
@@ -61,7 +42,6 @@
           settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
           persistencedSha256 = lib.fakeSha256;
         };
-        ###
       };
 
       programs.gamemode.enable = true;
--- modules/nixos/ollama/default.nix
+++ modules/nixos/ollama/default.nix
@@ -9,8 +9,9 @@
       services.ollama = {
         enable = true;
         acceleration = "cuda";
-        listenAddress = "0.0.0.0:11434";
-        # port = 11434;
+	host = "0.0.0.0";
+        port = 11434;
+        # listenAddress = "0.0.0.0:11434"; replaced by above two
       };
     };
 }
