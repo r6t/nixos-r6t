9aa262ac1d44e28650a28c0720673338953df008
r6t <ryancast@gmail.com>
2024-06-30 15:03:36 -0700

    nix got another one!

---
+++ hosts/photolab/configuration.nix
@@ -0,0 +1,54 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  outputs,
+  ...
+}:
+
+ {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+    ./hardware-configuration.nix
+    ../../modules/default.nix
+  ];
+
+  # system details
+  networking.hostName = "photo-lab";
+  networking.firewall.allowedTCPPorts = [ 
+    22
+    2283 # immich
+    ];
+  networking.firewall.allowedUDPPorts = [ 
+    ];
+  system.stateVersion = "23.11";
+  
+  # users.users.r6t.linger = true;
+
+  # system modules
+  mine.bolt.enable = true;
+  mine.bootloader.enable = true;
+  mine.docker.enable = true;
+  mine.env.enable = true;
+  mine.fwupd.enable = true;
+  mine.localization.enable = true;
+  mine.netdata.enable = true;
+  mine.networkmanager.enable = true;
+  mine.nix.enable = true;
+  mine.nixpkgs.enable = true;
+  mine.selfhost.enable = false;
+  mine.ssh.enable = true;
+  mine.syncthing.enable = true;
+  mine.tailscale.enable = true;
+  mine.thunderbay.enable = false;
+  mine.user.enable = true;
+  mine.zsh.enable = true;
+
+  # home modules
+  mine.home.awscli.enable = true;
+  mine.home.git.enable = true;
+  mine.home.home-manager.enable = true;
+  mine.home.python3.enable = true;
+  mine.home.zsh.enable = true;
+}
