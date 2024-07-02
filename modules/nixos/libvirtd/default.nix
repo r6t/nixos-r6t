748ae471e771004631b83e58017de1d8af58d2a5
r6t <ryancast@gmail.com>
2024-07-02 12:45:25 -0700

    libvirtd config

--- modules/nixos/libvirtd/default.nix
+++ modules/nixos/libvirtd/default.nix
@@ -13,9 +13,19 @@
         libvirtd = {
           enable = true;
           qemu.ovmf.enable = true;
+          qemu.package = pkgs.qemu_kvm;
+          qemu.options = [ "-L /run/current-system/sw/bin/virtiofsd" ];
         };
       };
       users.users.r6t.extraGroups = [ "libvirtd" ];
+      services.libvirtd = {
+        enable = true;
+        extraConfig = ''
+          unix_sock_group = "libvirt"
+          unix_sock_rw_perms = "0770"
+          auth_unix_rw = "none"
+        '';
+      };
       programs.virt-manager.enable = true;
   };
 }
