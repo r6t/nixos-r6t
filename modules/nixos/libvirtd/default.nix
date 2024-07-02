1bcbd2a1b7b7688a76a19302bc65186368621891
r6t <ryancast@gmail.com>
2024-07-02 12:49:09 -0700

    undo bad service

--- modules/nixos/libvirtd/default.nix
+++ modules/nixos/libvirtd/default.nix
@@ -13,19 +13,11 @@
         libvirtd = {
           enable = true;
           qemu.ovmf.enable = true;
-          qemu.package = pkgs.qemu_kvm;
-          qemu.options = [ "-L /run/current-system/sw/bin/virtiofsd" ];
+          package = pkgs.qemu_kvm;
+          extraOptions = [ "-L /run/current-system/sw/bin/virtiofsd" ];
         };
       };
       users.users.r6t.extraGroups = [ "libvirtd" ];
-      services.libvirtd = {
-        enable = true;
-        extraConfig = ''
-          unix_sock_group = "libvirt"
-          unix_sock_rw_perms = "0770"
-          auth_unix_rw = "none"
-        '';
-      };
       programs.virt-manager.enable = true;
   };
 }
