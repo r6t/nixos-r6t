fa1dca1cd74c1583100030d702d49a59f4ff63e5
r6t <ryancast@gmail.com>
2024-07-02 21:34:35 -0700

    fix name

--- modules/home/digikam/default.nix
+++ modules/home/digikam/default.nix
@@ -1,11 +1,11 @@
 { lib, config, pkgs, ... }: { 
 
     options = {
-      mine.home.darktable.enable =
-        lib.mkEnableOption "enable darktable in home-manager";
+      mine.home.digikam.enable =
+        lib.mkEnableOption "enable digikam in home-manager";
     };
 
-    config = lib.mkIf config.mine.home.darktable.enable { 
-      home-manager.users.r6t.home.packages = with pkgs; [ darktable ];
+    config = lib.mkIf config.mine.home.digikam.enable { 
+      home-manager.users.r6t.home.packages = with pkgs; [ digikam ];
     };
 }
