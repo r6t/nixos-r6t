f8ec0bb20fe739d74659bd18eb15363fa10c0e62
r6t <ryancast@gmail.com>
2024-09-17 09:56:50 -0700

    wip configure secondary package source one-off

--- modules/nixos/ollama/default.nix
+++ modules/nixos/ollama/default.nix
@@ -1,4 +1,12 @@
-{ lib, config, inputs, ... }: { 
+{ lib, config, inputs, ... }:let
+  # Perform an ad-hoc import for `nixpkgs-unstable` with the desired configuration.
+  nixpkgs-unstable = import inputs.nixpkgs-unstable {
+    config = { allowUnfree = true; };
+  };
+
+  # Define the package using the ad-hoc imported and configured `nixpkgs-unstable`
+  ollamaPackage = nixpkgs-unstable.legacyPackages.x86_64-linux.ollama;
+in { 
 
     options = {
       mine.ollama.enable =
@@ -9,7 +17,8 @@
       services.ollama = {
         enable = true;
         acceleration = "cuda";
-        package = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.ollama;
+        package = ollamaPackage;
+        # package = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.ollama;
         # host = "0.0.0.0"; # use older listenAddress on 24.05
         listenAddress = "0.0.0.0:11434";
         # port = 11434;
