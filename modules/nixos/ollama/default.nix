7f96a603b74d3a30bf2a87a6049a1d10e4e4e0aa
r6t <ryancast@gmail.com>
2024-09-17 10:50:27 -0700

    ollama from nixpkgs-unstable with extra import instead of overlay

--- modules/nixos/ollama/default.nix
+++ modules/nixos/ollama/default.nix
@@ -1,28 +1,23 @@
 { lib, config, inputs, ... }:let
-  # Perform an ad-hoc import for `nixpkgs-unstable` with the desired configuration.
+  # ad-hoc import nixpkgs-unstable to set config. I'd rather avoid the extra import but was having trouble getting it to work otherwise
   nixpkgs-unstable = import inputs.nixpkgs-unstable {
     system = "x86_64-linux";
     config = { allowUnfree = true; };
   };
-
-  # Define the package using the ad-hoc imported and configured `nixpkgs-unstable`
-  ollamaPackage = nixpkgs-unstable.legacyPackages.x86_64-linux.ollama;
+  ollamaPackage = nixpkgs-unstable.ollama;
 in { 
-
     options = {
       mine.ollama.enable =
         lib.mkEnableOption "enable ollama server (nvidia)";
     };
-
     config = lib.mkIf config.mine.ollama.enable { 
       services.ollama = {
         enable = true;
         acceleration = "cuda";
         package = ollamaPackage;
-        # package = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.ollama;
         # host = "0.0.0.0"; # use older listenAddress on 24.05
-        listenAddress = "0.0.0.0:11434";
         # port = 11434;
+        listenAddress = "0.0.0.0:11434";
       };
     };
 }
