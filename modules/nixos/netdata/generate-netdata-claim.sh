58b2c77675475f9d4c9a6929ffd2544841d6532a
r6t <ryancast@gmail.com>
2024-09-24 18:29:05 -0700

    token support

--- modules/nixos/netdata/generate-netdata-claim.sh
+++ modules/nixos/netdata/generate-netdata-claim.sh
@@ -3,4 +3,6 @@
 NETDATA_TOKEN=$(cat /run/secrets/netdata/cloud/claim_token)
 #NETDATA_ROOMS=$(cat /run/secrets/netdata/cloud/claim_rooms)
 
+mkdir -p /var/lib/netdata/cloud.d
+touch /var/lib/netdata/cloud.d/token
 echo "$NETDATA_TOKEN" > /var/lib/netdata/cloud.d/token
