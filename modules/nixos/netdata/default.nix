e23f2b301fff546ac3a51fe03d21e6dfbf60e1ec
r6t <ryancast@gmail.com>
2024-09-24 21:52:45 -0700

    trying self-hosted instead of netdata cloud

--- modules/nixos/netdata/default.nix
+++ modules/nixos/netdata/default.nix
@@ -2,9 +2,9 @@
   # ad-hoc import nixpkgs-unstable to set config. I'd rather avoid the extra import but was having trouble getting it to work otherwise
   nixpkgs-unstable = import inputs.nixpkgs-unstable {
     system = "x86_64-linux";
-    config = { allowUnfree = true; };
+  #  config = { allowUnfree = true; };
   };
-  netdataPackage = nixpkgs-unstable.netdataCloud;
+  netdataPackage = nixpkgs-unstable.netdata;
 in { 
     options = {
       mine.netdata.enable =
@@ -14,25 +14,7 @@ in {
     config = lib.mkIf config.mine.netdata.enable { 
       services.netdata = {
         enable = true;
-        # token file gets deleted during activation, subsequent reloads fail
-        # claimTokenFile = /var/lib/netdata/cloud.d/token;
         package = netdataPackage;
-      };
-
-      systemd.services = {
-        netdata.unitConfig = {
-          Requires = [ "generate-netdata-claim.service" ];
-          After = [ "generate-netdata-claim.service" ];
-        };
-        "generate-netdata-claim" = {
-          enable = true;
-          script = builtins.readFile ./generate-netdata-claim.sh;
-          wantedBy = [ "multi-user.target" ];
-          unitConfig = {
-            PartOf = [ "netdata.service" ];
-            Before = [ "netdata.service" ];
-          };
-        };
       };
     };
 }
