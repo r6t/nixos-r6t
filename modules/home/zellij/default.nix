fde0384a92db0248e8f52bc3b5c5029e8888c9d2
r6t <git@r6t.io>
2024-10-14 17:43:50 -0700

    zellij colorscheme

--- modules/home/zellij/default.nix
+++ modules/home/zellij/default.nix
@@ -9,6 +9,30 @@
     home-manager.users.r6t.programs.zellij = {
       enable = true;
       enableZshIntegration = true;
+      settings = {
+	theme = "oxocarbon";
+	themes.oxocarbon = {
+          fg = "#ffffff";
+          bg = "#161616";
+          black = "#262626";
+          red = "#ee5396";
+          green = "#42be65";
+          yellow = "#ffe97b";
+          blue = "#33b1ff";
+          magenta = "#ff7eb6";
+          cyan = "#3ddbd9";
+          white = "#dde1e6";
+          bright_black = "#393939";
+          bright_red = "#ee5396";
+          bright_green = "#42be65";
+          bright_yellow = "#ffe97b";
+          bright_blue = "#33b1ff";
+          bright_magenta = "#ff7eb6";
+          bright_cyan = "#3ddbd9";
+          bright_white = "#ffffff";
+	  orange = "#ffdab9"; # zellij threw an error requiring orange, this isn't part of oxocarbon
+        };
+      };
     };
   };
 }
