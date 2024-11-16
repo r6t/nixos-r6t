d3ea46322f60bd1f922e540491cd132bd34a27bd
r6t <git@r6t.io>
2024-11-15 17:51:35 -0800

    move nvidia gpu to saguaro

--- modules/nixos/nvidia/default.nix
+++ modules/nixos/nvidia/default.nix
@@ -9,52 +9,40 @@
       
       boot.kernelParams = [ 
         "nvidia-drm.modeset=1" # troubleshooting gpu fan bottoming out at 30
-        "nvidia-drm.fbdev=1"  # troubleshooting plasma 6 black screen
         "nvidia.NVreg_PreserveVideoMemoryAllocations=1"  # sleep/wake
         ];
 
-      environment.sessionVariables = {
-        # Used these with hyprland, now using plasma 6
-        # GBM_BACKEND = "nvidia-drm";
-        # LIBVA_DRIVER_NAME = "nvidia";
-        # WLR_NO_HARDWARE_CURSORS = "1";
-        # XDG_SESSION_TYPE = "wayland";
-        # __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      };
       environment.systemPackages = with pkgs; [ libva ];
 
-      # replaces hardware.opengl after nixos 24.05
       hardware.graphics = {
         enable = true;
         enable32Bit = true;
       };
 
-      # the old way
-      # hardware.opengl = {
-      #   enable = true;
-      #   driSupport = true;
-      #   driSupport32Bit = true;
-      # };
-      
       hardware.nvidia = {
         modesetting.enable = true;
         powerManagement.enable = true; # changed from default false
         open = false;
         nvidiaSettings = true;
-        # package = config.boot.kernelPackages.nvidiaPackages.stable;
         package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-          version = "555.42.02";
-          sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
-          sha256_aarch64 = lib.fakeSha256;
-          openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-          settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+          version = "555.58.02";
+          sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+          sha256_aarch64 = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+          openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+          settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
           persistencedSha256 = lib.fakeSha256;
         };
+      #package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      #    version = "555.42.02";
+      #    sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
+      #    sha256_aarch64 = lib.fakeSha256;
+      #    openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+      #    settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+      #    persistencedSha256 = lib.fakeSha256;
+      #  };
       };
 
       hardware.nvidia-container-toolkit.enable = true;
-
-      programs.gamemode.enable = true;
 
       services.xserver = {
         videoDrivers = ["nvidia"];
