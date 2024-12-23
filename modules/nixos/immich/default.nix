e549f4f058d43379a08ca9a9dbaf900b7c92f65e
r6t <git@r6t.io>
2024-12-23 12:12:25 -0800

    immich external resource config

--- modules/nixos/immich/default.nix
+++ modules/nixos/immich/default.nix
@@ -11,16 +11,24 @@
       host = "0.0.0.0";
       port = 2283;
       mediaLocation = "/mnt/thunderbay/4TB-B/immich";
+      environment = {
+        IMMICH_MACHINE_LEARNING_URL = lib.mkForce "http://100.64.0.27:3003";
+      };
       settings = {
         UPLOAD_LOCATION = "/mnt/thunderbay/4TB-B/immich/upload";
         THUMB_LOCATION = "/mnt/thunderbay/4TB-B/immich/thumbs";
         ENCODED_VIDEO_LOCATION = "/mnt/thunderbay/4TB-B/immich/encoded-video";
         PROFILE_LOCATION = "/mnt/thunderbay/4TB-B/immich/profile";
         BACKUP_LOCATION = "/mnt/thunderbay/4TB-B/immich/backups";
+        IMMICH_MACHINE_LEARNING_URL = lib.mkForce "http://100.64.0.27:3003";
       };
     };
-    systemd.services.immich.after = [ "thunderbay.service" ];
-    systemd.services.immich.requires = [ "thunderbay.service" ];
-
+    systemd.services.immich = {
+      after = [ "tailscaled.service" "thunderbay.service" ];
+      requires = [ "thunderbay.service" ];
+      serviceConfig = {
+        ReadOnlyPaths = [ "/mnt/thunderbay/4TB-B/Pictures/" ];
+      };
+    };
   };
 }
