43d3ba6aced1b455c7f3a503de4b820502f9a5db
r6t <git@r6t.io>
2025-02-18 09:05:50 -0800

    initial prometheus scrape config

--- modules/nixos/prometheus/default.nix
+++ modules/nixos/prometheus/default.nix
@@ -10,6 +10,20 @@
     services.prometheus = {
       enable = true;
       port = 9001;
+      exporters = {
+	node = {
+	  enable = true;
+	  enabledCollectors = [ "systemd" ];
+	  port = 9002; # stringified and referenced in target config
+	};
+      };
+      scrapeConfigs = [
+	{
+	  job_name = "moon";
+	  static_configs = [{
+	    targets = [ "moon:${toString config.services.prometheus.exporters.node.port}" ];
+	  }];
+	}];
     };
 
     environment.systemPackages = with pkgs; [ prometheus ];
