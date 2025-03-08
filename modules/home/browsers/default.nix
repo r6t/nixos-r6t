1644806e9297ea1b131d34d09c7562bf7a73046f
r6t <git@r6t.io>
2025-03-08 09:30:34 -0800

    cleaner firefox module

--- modules/home/browsers/default.nix
+++ modules/home/browsers/default.nix
@@ -17,65 +17,196 @@
         enable = true;
         profiles."default" = {
           id = 0;
+          search = {
+            default = "Searxng";
+            force = true;
+            engines = {
+              "Searxng" = {
+                urls = [{
+                  template = "https://searxng.r6t.io/search";
+                  params = [
+                    { name = "q"; value = "{searchTerms}"; }
+                  ];
+                }];
+                iconUpdateURL = "https://searxng.r6t.io/favicon.ico";
+                updateInterval = 24 * 60 * 60 * 1000; # Daily
+                definedAliases = [ "@sx" ];
+              };
+              "DuckDuckGo".metaData.hidden = false;
+              "Google".metaData.hidden = true;
+              "Amazon.com".metaData.hidden = true;
+              "Bing".metaData.hidden = true;
+              "eBay".metaData.hidden = true;
+            };
+          };
           settings = {
-            "app.shield.optoutstudies.enabled" = false;
-            "browser.crashReports.enabled" = false;
-            "browser.discovery.enabled" = false;
-            "browser.download.dir" = "$HOME/Downloads";
-            "browser.download.folderList" = 2;
-            "browser.newtab.url" = "about:blank";
-            "browser.newtabpage.enabled" = false;
-            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-            "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
-            "browser.newtabpage.activity-stream.feeds.discoverystreamfeed" = false;
-            "browser.preferences.defaultPerformanceSettings.enabled" = false; # Do not use recommended performance settings
-            "browser.search.defaultenginename" = "DuckDuckGo";
-            "browser.search.suggest.enabled" = false; # Disable search suggestions
-            "browser.urlbar.suggest.searches" = false; # Additional setting to disable search suggestions in the address bar.
-            "browser.search.showOneOffButtons" = false; # Possibly relevant for disabling quick-search buttons for other search engines.
-            "browser.newtabpage.activity-stream.showSponsored" = false; # Disable sponsored content on new tab page
-            "browser.startup.homepage" = "about:blank";
-            "browser.startup.page" = 0; # about:blank in new windows
-            "browser.urlbar.placeholderName" = "DuckDuckGo";
-            "browser.urlbar.placeholderName.private" = "DuckDuckGo";
-            "datareporting.healthreport.uploadEnabled" = false;
-            "dom.security.https_only_mode" = true; # Enable HTTPS-Only Mode
-            "extensions.getAddons.showPane" = false;
-            "extensions.noNewNetPrefsMigrate" = true; # Prevent migration of new network preferences from extensions
-            "general.smoothScroll" = true;
-            "identity.sync.enabled" = true;
-            "identity.sync.engine.addons" = true;
-            "identity.sync.engine.addresses" = true;
-            "identity.sync.engine.creditcards" = true;
-            "identity.sync.engine.extensions" = true;
-            "identity.sync.engine.forms" = true;
-            "identity.sync.engine.history" = true;
-            "identity.sync.engine.passwords" = true;
-            "identity.sync.engine.payments" = true;
-            "identity.sync.engine.prefs" = true;
-            "identity.sync.engine.tabs" = true;
-            "identity.sync.username" = "$(cat /run/secrets/firefox_sync)";
-            "layers.acceleration.disabled" = true; # Disable hardware acceleration
-            "network.cookie.cookieBehavior" = 1; # Block all cross-site cookies
-            "network.cookie.cookiePrefetch" = 0; # Disable cookie prefetching
-            "network.cookie.lifetimePolicy" = 2; # Session cookies only (delete on close)
-            "privacy.clearOnShutdown.cache" = true;
-            "privacy.clearOnShutdown.cookies" = true;
-            "privacy.clearOnShutdown.formData" = false;
-            "privacy.clearOnShutdown.history" = false;
-            "privacy.cpd.enabled" = true; # Enable Content Preference for DOM storage
-            "privacy.cpd.excludedDomains" = ""; # Add domains you want to exclude from strict mode if needed
-            "privacy.donottrackheader.enabled" = true;
-            "privacy.restrict3rdpartycookies" = true; # Restrict third-party cookies
-            "privacy.trackingprotection.cryptomining.enabled" = false;
-            "privacy.trackingprotection.enabled" = true;
-            "privacy.trackingprotection.socialmedia.enabled" = false;
-            "signon.rememberSignons" = false; # Disable Firefox credential store
-            "toolkit.telemetry.enabled" = false;
-            "ui.systemUsesDarkTheme" = 1;
+            # App settings
+            app = {
+              shield.optoutstudies.enabled = false;
+              normandy = {
+                api_url = "";
+                enabled = false;
+              };
+            };
+
+            # Browser settings
+            browser = {
+              crashReports.enabled = false;
+              discovery.enabled = false;
+              download = {
+                dir = "$HOME/Downloads";
+                folderList = 2;
+              };
+              newtab.url = "https://uptime.r6t.io/status/core";
+              newtabpage = {
+                enabled = false;
+                activity-stream = {
+                  showSponsoredTopSites = false;
+                  feeds.section.topstories = false;
+                  feeds.discoverystreamfeed = false;
+                  showSponsored = false;
+                };
+              };
+              preferences.defaultPerformanceSettings.enabled = false;
+              search = {
+                defaultenginename = "Searxng";
+                suggest.enabled = false;
+                showOneOffButtons = false;
+              };
+              startup = {
+                homepage = "https://uptime.r6t.io/status/core";
+                page = 0;
+              };
+              urlbar = {
+                placeholderName = "Searxng";
+                suggest = {
+                  searches = false;
+                  history = false;
+                  bookmark = false;
+                  openpage = false;
+                  topsites = false;
+                };
+              };
+              formfill.enable = false;
+              ping-centre.telemetry = false;
+              tabs.crashReporting.sendReport = false;
+            };
+
+            # Data reporting settings
+            datareporting = {
+              healthreport.uploadEnabled = false;
+              policy.dataSubmissionEnabled = false;
+            };
+
+            # DOM settings
+            dom.security.https_only_mode = true;
+
+            # Extensions settings
+            extensions = {
+              getAddons.showPane = false;
+              noNewNetPrefsMigrate = true;
+              pocket.enabled = false;
+              formautofill = {
+                addresses.enabled = false;
+                creditCards.enabled = false;
+              };
+            };
+
+            # General settings
+            general.smoothScroll = true;
+
+            # Identity settings
+            identity.sync = {
+              enabled = true;
+              username = "$(cat /run/secrets/firefox_sync)";
+              engine = {
+                addons = true;
+                addresses = true;
+                creditcards = true;
+                extensions = true;
+                forms = true;
+                history = true;
+                passwords = true;
+                payments = true;
+                prefs = true;
+                tabs = true;
+              };
+            };
+
+            # Layers settings
+            layers.acceleration.disabled = true;
+
+            # Media settings
+            media.peerconnection.enabled = false;
+
+            # Network settings
+            network = {
+              cookie = {
+                cookieBehavior = 1;
+                cookiePrefetch = 0;
+                lifetimePolicy = 2;
+              };
+              dns.disablePrefetch = true;
+              prefetch-next = false;
+              predictor = {
+                enabled = false;
+                enable-prefetch = false;
+              };
+            };
+
+            # Privacy settings
+            privacy = {
+              clearOnShutdown = {
+                cache = true;
+                cookies = true;
+                formData = false;
+                history = false;
+              };
+              cpd = {
+                enabled = true;
+                excludedDomains = "";
+              };
+              donottrackheader.enabled = true;
+              restrict3rdpartycookies = true;
+              trackingprotection = {
+                cryptomining.enabled = true;
+                enabled = true;
+                socialmedia.enabled = true;
+                fingerprinting.enabled = true;
+              };
+              firstparty.isolate = true;
+              resistFingerprinting = true;
+              partition.network_state = true;
+            };
+
+            # Sign-on settings
+            signon.rememberSignons = false;
+
+            # Toolkit settings
+            toolkit.telemetry = {
+              enabled = false;
+              archive.enabled = false;
+              bhrPing.enabled = false;
+              firstShutdownPing.enabled = false;
+              newProfilePing.enabled = false;
+              reportingpolicy.firstRun = false;
+              shutdownPingSender.enabled = false;
+              unified = false;
+              updatePing.enabled = false;
+            };
+
+            # UI settings
+            ui.systemUsesDarkTheme = 1;
+
+            # Geo settings
+            geo.enabled = false;
+
+            # Breakpad settings
+            breakpad.reportURL = "";
           };
         };
       };
     };
   };
 }
+
