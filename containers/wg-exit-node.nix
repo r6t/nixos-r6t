a58ec4eeaf3b77b9e420f866b874ff0affdd0381
r6t <git@r6t.io>
2026-02-01 21:11:25 -0800

    wg-exit-node include tailscale

--- containers/wg-exit-node.nix
+++ containers/wg-exit-node.nix
@@ -3,6 +3,7 @@
     ./r6-lxc-base.nix
     ./r6-lxc-mullvad-dns-add-on.nix
     ../modules/nixos/exit-node-routing/default.nix
+    ../modules/nixos/tailscale/default.nix
   ];
 
   mine.exit-node-routing.enable = true;
