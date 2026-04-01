007b75c7082b86799d8cf5373c0d73145db11ca6
r6t <git@r6t.io>
2026-04-01 16:03:34 -0700

    formatting

--- modules/home/alacritty/default.nix
+++ modules/home/alacritty/default.nix
@@ -12,13 +12,14 @@ let
           # macOS handles decorations differently
           decorations = if pkgs.stdenv.isDarwin then "buttonless" else "none";
         };
-        terminal.shell = if pkgs.stdenv.isDarwin then {
-          program = "${pkgs.zellij}/bin/zellij";
-        } else {
-          # Wrap in systemd scope so zellij server survives alacritty exit
-          program = "${pkgs.systemd}/bin/systemd-run";
-          args = [ "--scope" "--user" "${pkgs.zellij}/bin/zellij" ];
-        };
+        terminal.shell =
+          if pkgs.stdenv.isDarwin then {
+            program = "${pkgs.zellij}/bin/zellij";
+          } else {
+            # Wrap in systemd scope so zellij server survives alacritty exit
+            program = "${pkgs.systemd}/bin/systemd-run";
+            args = [ "--scope" "--user" "${pkgs.zellij}/bin/zellij" ];
+          };
         colors = {
           primary = {
             background = "#161616";
