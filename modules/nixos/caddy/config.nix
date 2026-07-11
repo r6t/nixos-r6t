6851f7fc4af1aa123598afa947d5d240a3d7381d
r6t <git@r6t.io>
2026-07-10 20:32:54 -0700

    bump caddy sha

--- modules/nixos/caddy/config.nix
+++ modules/nixos/caddy/config.nix
@@ -25,7 +25,7 @@ in
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/route53@v1.6.0" ];
-        hash = "sha256-b0lJLOCgFzbxsahXmYyqcDMRv1OEvNWvuDkqU5DfrPg=";
+        hash = "sha256-4Q/+ETXHbAi4HGXK+6JreHO1mqPmoK2CcVSSAqDgVhY=";
       };
     }
 
